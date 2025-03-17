446e76e4a37122b7f1f11f6ddeeaf11a36bbf8e9
Ben <ben.y.chang@outlook.com>
2025-03-17 11:04:41 +0000

    small addition

--- dft/sine_wave_sum.py
+++ dft/sine_wave_sum.py
@@ -13,12 +13,18 @@ sin2 = np.sin((x * 2 * np.pi) * freq2)
 sample = np.linspace(0, 1, 20)
 sampled = np.sin((sample * 2 * np.pi) * freq1) + np.sin((sample * 2 * np.pi) * freq2)
 
+# Showing the signal as a sum of 2 simpler signals
 plt.plot(x, sin1, c="green", alpha=0.4)
 plt.plot(x, sin2, c="orange", alpha=0.4)
 plt.plot(x, sin1 + sin2, c="black")
 
+# Showing how it is sampled
 plt.figure()
 plt.plot(x, sin1 + sin2, c="black")
 plt.scatter(sample, sampled, c="red")
+
+# Showing the sampled signal
+plt.figure()
+plt.plot(sample, sampled, c="black")
 
 plt.show()
