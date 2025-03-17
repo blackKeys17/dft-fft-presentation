1c5254ecb4bd544554f3c99204b36bab558a57af
Ben <ben.y.chang@outlook.com>
2025-03-17 11:21:07 +0000

    added constant signal

--- dft/different_frequency_waves.py
+++ dft/different_frequency_waves.py
@@ -6,15 +6,15 @@ x = np.linspace(0, 1, 1000)
 
 # How many samples?
 n = 20
-fig, ax = plt.subplots(n, 1)
+fig, ax = plt.subplots(n+1, 1)
 
 # For gradient
-grad = np.linspace(0, 1, n)
+grad = np.linspace(0, 1, n+1)
 
 # Plotting pure sine waves for each frequency
-for i in range(1, n+1):
+for i in range(0, n+1):
     y = sin1 = np.sin((x * 2 * np.pi) * i)
-    ax[i-1].plot(x, y, c=(grad[i-1], 0, 0))
-    ax[i-1].set_axis_off()
+    ax[i].plot(x, y, c=(grad[i], 0, 0))
+    ax[i].set_axis_off()
 
 plt.show()
